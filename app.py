0d0db59e48b9599289dfd3158174d4ff55aa524e
Aline Schneider <alinesviana@hotmail.com>
2020-05-04 22:19:18 -0400

    Updating app.py, section 9.5.1.

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
 # Import the Flask Dependency
-from flask import Flask
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
 
-# Create a New Flask App Instance
+# Prepare the database file to be connected to later on
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# Reflect the database into our classes
+Base = automap_base()
+
+# Reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save our references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create a session link from Python to our database
+session = Session(engine)
+
+# Define our Flask app, creating a New Flask App Instance
 app = Flask(__name__)
 
 # Create Flask Routes
