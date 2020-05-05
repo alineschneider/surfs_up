a3e46de636afc11dcd02d7df669acc00331dd2ff
Aline Schneider <alinesviana@hotmail.com>
2020-05-05 18:30:23 -0400

    Updating app.py, section 9.5.6.

--- app.py
+++ app.py
@@ -28,6 +28,66 @@ session = Session(engine)
 app = Flask(__name__)
 
 # Create Flask Routes
+# Welcome Route
 @app.route('/')
-def hello_world():
-	return 'Hello world'
+def welcome():
+    return(
+    """Welcome to the Climate Analysis API!\n
+    Available Routes:\n
+    api/v1.0/precipitation\n
+    api/v1.0/stations\n
+    api/v1.0/tobs\n
+    api/v1.0/temp/start/end
+    """)
+
+# Precipitation Route
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+	   filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
+
+# Stations Route
+@app.route("/api/v1.0/stations")
+def stations():
+   results = session.query(Station.station).all()
+   stations = list(np.ravel(results))
+   return jsonify(stations)
+
+# Monthly Temperature Route
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   results = session.query(Measurement.tobs).\
+	   filter(Measurement.station == 'USC00519281').\
+	   filter(Measurement.date >= prev_year).all()
+   temps = list(np.ravel(results))
+   return jsonify(temps)
+
+# Statistics Route
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+def stats(start=None, end=None):
+# 	sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]           
+# 	if not end:
+# 		results = session.query(*sel).filter(Measurement.date <= start).all()
+# 		temps = list(np.ravel(results))
+# 		return jsonify(temps)
+
+#     results = session.query(*sel).\
+# 		filter(Measurement.date >= start).\
+# 			filter(Measurement.date <= end).all()
+#     temps = list(np.ravel(results))
+#     return jsonify(temps)
+
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+    if not end:
+        results = session.query(*sel).filter(Measurement.date <= start).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+    results = session.query(*sel).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps)
+
